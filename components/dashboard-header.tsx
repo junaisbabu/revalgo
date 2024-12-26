f4cb4f4f067a62e5966da9dd96dbbadcde7e6999
Junais Babu <junaisdvs@gmail.com>
2024-12-26 22:32:20 +0530

    delete breadcrumb

---
+++ components/dashboard-header.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+export function DashboardHeader() {
+  const pathname = usePathname();
+
+  const title = pathname.split("/").filter(Boolean)[0];
+
+  return (
+    <header className="w-full py-2 px-6 h-16 flex items-center justify-between">
+      <h1 className="capitalize text-lg font-bold">{title || "Home"}</h1>
+      <div>
+        <Image
+          className="rounded-full"
+          src="https://github.com/shadcn.png"
+          alt="User"
+          width={36}
+          height={36}
+        />
+      </div>
+    </header>
+  );
+}
